43819979724fa7f02fdde7045b6544847439969c
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-14 02:32:52 -0700

    Initial Commit

---
+++ iKnowYou/iKnowYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKnowYouApp.swift
+//  iKnowYou
+//
+//  Created by Kenneth Oliver Rathbun on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct iKnowYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
