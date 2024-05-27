635ecbf69d63afe5c009cd4421a91144a17ce4a3
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-27 00:18:35 -0700

    Refactored PhotoCard

--- iKnowYou/PhotoCard.swift
+++ iKnowYou/PhotoCard.swift
@@ -7,41 +7,34 @@
 
 import SwiftUI
 import SwiftData
-
 import CoreImage
 import PhotosUI
 import StoreKit
-
 import MapKit
 
 struct PhotoCard: View {
     var friend: Friend
-    @State private var title: String?
-    
+    @State private var title: String
     @State private var selectedImage: PhotosPickerItem?
     @State private var imageData: Data?
     @State private var image: Image?
- 
     @State private var location: Location?
     
     init(friend: Friend) {
         self.friend = friend
         _title = State(initialValue: friend.name)
         _imageData = State(initialValue: friend.image)
-        loadImage()
         _location = State(initialValue: friend.location)
     }
     
     var startPosition: MapCameraPosition? {
         if let coordinate = location?.coordinate.clLocationCoordinate2D {
-            let position = MapCameraPosition.region(
-                MKCoordinateRegion (
+            return MapCameraPosition.region(
+                MKCoordinateRegion(
                     center: coordinate,
                     span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
                 )
             )
-            
-            return position
         } else {
             return nil
         }
@@ -59,60 +52,48 @@ struct PhotoCard: View {
                 }
             }
             .onAppear {
-                Task {
-                    guard let imageData, let uiImage = UIImage(data: imageData) else { return }
+                if let imageData, let uiImage = UIImage(data: imageData) {
                     image = Image(uiImage: uiImage)
                 }
             }
-            .onChange(of: selectedImage, loadImage)
+            .onChange(of: selectedImage) {
+                Task {
+                    image = await ImageLoader.loadImage(from: selectedImage)
+                }
+            }
             
-            Text(title ?? "New Friend")
+            Text(title)
                 .font(.title)
-                .foregroundStyle(.white)
+                .foregroundColor(.white)
                 .padding()
                 .frame(maxWidth: .infinity)
-                .background(.blue)
+                .background(Color.blue)
             
-            if let startPosition {
+            if let startPosition = startPosition {
                 Map(initialPosition: startPosition)
                     .frame(height: 200)
                     .padding()
             } else {
                 Text("No Location.")
             }
-            
         }
-        .clipShape(.rect(cornerRadius: 10))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
         .overlay {
             RoundedRectangle(cornerRadius: 10)
-                .stroke(.secondary, lineWidth: 3)
+                .stroke(Color.secondary, lineWidth: 3)
         }
         .padding()
     }
-    
-    func loadImage() {
-        Task {
-            guard
-                let data = try await selectedImage?.loadTransferable(type: Data.self),
-                let uiImage = UIImage(data: data)
-            else {
-                return
-            }
-            
-            let loadedImage = Image(uiImage: uiImage)
-            image = loadedImage
-        }
-    }
 }
 
-#Preview {
-    do {
-        let config = ModelConfiguration(isStoredInMemoryOnly: true)
-        let container = try ModelContainer(for: Friend.self, configurations: config)
-        
-        return PhotoCard(friend: Friend.example)
-            .modelContainer(container)
-    } catch {
-        return Text("Failed to create preview: \(error.localizedDescription)")
-    }
-}
+//#Preview {
+//    do {
+//        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+//        let container = try ModelContainer(for: Friend.self, configurations: config)
+//        
+//        return PhotoCard(friend: Friend.example)
+//            .modelContainer(container)
+//    } catch {
+//        return Text("Failed to create preview: \(error.localizedDescription)")
+//    }
+//}
